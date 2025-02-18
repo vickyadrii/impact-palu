a6b0328e505579eda06e4a2df0dc80fc0adf33f1
vickyadri29 <vickyadri290501@gmail.com>
2025-02-18 20:13:58 +0700

    chore: adjust the gap between portfolios and programs

--- src/features/programs/components/portfolio-cards.tsx
+++ src/features/programs/components/portfolio-cards.tsx
@@ -3,7 +3,7 @@ import PortfolioItemCard from "./portfolio-item-card";
 
 const PortfolioCards = () => {
   return (
-    <div className="space-y-8">
+    <div className="space-y-8 mt-8">
       {portfolios.map((portfolio) => (
         <PortfolioItemCard key={portfolio.id} portfolio={portfolio} />
       ))}
--- src/features/programs/index.tsx
+++ src/features/programs/index.tsx
@@ -6,14 +6,16 @@ import ProgramTitle from "./components/program-title";
 const Programs = () => {
   return (
     <div className="container mx-auto p-5">
-      <div className="flex flex-col gap-12">
+      <div className="flex flex-col gap-16">
         <div>
           <ProgramTitle />
           <ProgramCards />
         </div>
 
-        <PortfolioTitle />
-        <PortfolioCards />
+        <div>
+          <PortfolioTitle />
+          <PortfolioCards />
+        </div>
       </div>
     </div>
   );
