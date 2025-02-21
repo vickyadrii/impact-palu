56a19b07319d9249bb0f1f5bf2b5e1fc4c10b1a4
vickyadrii <vickyadri103@gmail.com>
2025-02-21 11:02:18 +0700

    chore: adjust the layout in events page

--- src/features/events-opportunities/components/opportunity-cards.tsx
+++ src/features/events-opportunities/components/opportunity-cards.tsx
@@ -5,7 +5,7 @@ const OpportunityCard = () => {
   return (
     <div className="space-y-6">
       <h3 className="font-semibold md:text-xl text-lg">OPPORTUNITIES</h3>
-      <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-x-4 gap-y-6">
+      <div className="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-x-4 gap-y-6">
         {opportunities.map((opportunity) => (
           <OpportunitiyItemCard key={opportunity.id} opportunity={opportunity} />
         ))}
